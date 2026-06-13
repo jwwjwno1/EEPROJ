a7c6e8b751ae7d52d553b42983c1062dc67ce4ad
jwwjwno1 <jwwjwno1@gmail.com>
2026-06-13 13:37:51 +0800

    fix: production build successful

--- my-app/app/api/appointments/[id]/route.ts
+++ my-app/app/api/appointments/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 import { prisma } from "../../../lib/prisma";
-import { Prisma } from "@prisma/client";
+
 
 const getPrisma = () => {
   if (!prisma) return null;
